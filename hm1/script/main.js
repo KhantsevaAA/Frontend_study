d611ee781cb4bc00f1a530ed8205da905ae6273a
KhantsevaAA <3104nastya@gmail.com>
2022-10-12 12:01:47 +0300

    add first frontend

---
+++ hm1/script/main.js
@@ -0,0 +1,7 @@
+
+var myHeading = "фигня";
+alert(myHeading);
+document.write('НЯЯЯЯ!');
+function my_click(){
+    document.getElementById("image").src="images/5olx1r9Wlqs.jpg";
+};
---
+++ lesson1/index.js
@@ -0,0 +1,16 @@
+const variable=10;
+switch(variable){
+    case 11:{
+        console.log(10);
+        break;
+    }
+    case 12:{
+        console.log(10);
+        break;
+    }
+    default:
+        {
+            console.log('default');
+            break;
+        }
+}
